e46e61a7e809e3e13c7c29e0bededd20de4da9c4
yiyi <chen25924545@gmail.com>
2022-11-18 17:31:39 +0800

    js

--- js/weborphone.js
+++ js/weborphone.js
@@ -1,8 +1,8 @@
 var screenWidth = screen.width;
 const pageWidth  = document.documentElement.scrollWidth;
 var windowWidth = window.innerWidth;
-if(pageWidth< 1200 || windowWidth<1200 || screenWidth<1200){
-	window.location.href='https://ottoyiyi.github.io/pages/air%20quality%20monitor.html';
-}else{
+if(pageWidth>900 || windowWidth>900 || screenWidth>900){
 	window.location.href='https://ottoyiyi.github.io/pages/air%20quality%20monitor%20pc.html';
+}else{
+	window.location.href='https://ottoyiyi.github.io/pages/air%20quality%20monitor.html';
 }
