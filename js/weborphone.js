cf56e797d28cae43693b3c253fa2a3a6a38522e3
yiyi <chen25924545@gmail.com>
2022-11-18 17:15:08 +0800

    js

--- js/weborphone.js
+++ js/weborphone.js
@@ -1,5 +1,6 @@
 const pageWidth  = document.documentElement.scrollWidth;
-if(pageWidth< 900){
+var windowWidth = window.innerWidth;
+if(pageWidth< 900 ||windowWidth<900){
 	window.location.href='air quality monitor.html';
 }else{
 	window.location.href='air quality monitor pc.html';
